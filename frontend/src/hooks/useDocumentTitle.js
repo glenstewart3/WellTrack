36f7295a4fbc9b78011da17cebcfbd7bc727e05a
glenstewart3 <glen.stewart3@education.vic.gov.au>
2026-04-28 12:11:30 +1000

    Delete frontend directory

--- frontend/src/hooks/useDocumentTitle.js
+++
@@ -1,9 +0,0 @@
-import { useEffect } from 'react';
-
-export default function useDocumentTitle(title) {
-  useEffect(() => {
-    const prev = document.title;
-    document.title = title ? `${title} | WellTrack` : 'WellTrack';
-    return () => { document.title = prev; };
-  }, [title]);
-}
